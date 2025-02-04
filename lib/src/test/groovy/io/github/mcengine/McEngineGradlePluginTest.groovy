281350afb26b8904c341e8728a3591d0e4c377e3
JetsadaWijit <jetsadawijit@outlook.com>
2025-02-04 07:41:30 +0000

    Fix for build

--- lib/src/test/groovy/io/github/mcengine/McEngineGradlePluginTest.groovy
+++ lib/src/test/groovy/io/github/mcengine/McEngineGradlePluginTest.groovy
@@ -6,10 +6,15 @@ import spock.lang.Specification
 class McEngineGradlePluginTest extends Specification {
     def "plugin applies correctly"() {
         given:
-        def project = ProjectBuilder.builder().build()
-
+        def project = ProjectBuilder.builder().withProjectDir(new File('lib')).build()
+        
         when:
-        project.pluginManager.apply('io.github.mcengine.gradle-plugin')
+        try {
+            project.pluginManager.apply('io.github.mcengine.gradle-plugin')
+        } catch (Exception e) {
+            e.printStackTrace() // Print error details
+            throw e
+        }
 
         then:
         project.ext.has("getProjectVersion")
